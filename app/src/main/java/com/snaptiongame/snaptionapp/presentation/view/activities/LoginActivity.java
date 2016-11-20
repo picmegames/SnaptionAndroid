9c67c9a265c3703ddfc51f60f9557c62fbf72341
Tyler Wong <tbwong3@gmail.com>
2016-11-20 00:36:26 -0800

    Streamlined auth process with AuthenticationManager, added callbacks for login success

--- app/src/main/java/com/snaptiongame/snaptionapp/presentation/view/activities/LoginActivity.java
+++ app/src/main/java/com/snaptiongame/snaptionapp/presentation/view/activities/LoginActivity.java
@@ -1,27 +1,16 @@
 package com.snaptiongame.snaptionapp.presentation.view.activities;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.snaptiongame.snaptionapp.R;
-import com.snaptiongame.snaptionapp.data.auth.GoogleApiClientService;
+import com.snaptiongame.snaptionapp.data.auth.AuthenticationManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -39,22 +28,17 @@ public class LoginActivity extends AppCompatActivity {
    @BindView(R.id.google_sign_in_button)
    SignInButton mGoogleSignInButton;
 
-   private CallbackManager mCallbackManager;
-   private GoogleApiClient mGoogleApiClient;
-   private SharedPreferences mPreferences;
+   private AuthenticationManager mAuthManager;
 
    private static final int RC_SIGN_IN = 2222;
-
-   public static final String LOGGED_IN = "logged in";
-   public static final String FACEBOOK_LOGIN = "facebook";
-   public static final String GOOGLE_SIGN_IN = "google";
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
 
-      // Init Facebook SDK
-      FacebookSdk.sdkInitialize(getApplicationContext());
+      // Initialize Authentication Manager
+      mAuthManager = AuthenticationManager.getInstance(getApplicationContext());
+      mAuthManager.registerCallback(this::onBackPressed);
 
       setContentView(R.layout.activity_login);
       ButterKnife.bind(this);
@@ -69,81 +53,18 @@ public class LoginActivity extends AppCompatActivity {
             .fitCenter()
             .into(mLogo);
 
-      // Get Shared Preferences Editor
-      mPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+      mAuthManager.setFacebookCallback(this, mFacebookLoginButton);
+   }
 
-      // Init Facebook Login Callbacks
-      FacebookSdk.sdkInitialize(getApplicationContext());
-      mCallbackManager = CallbackManager.Factory.create();
-
-      // Set Facebook Login Permissions
-      mFacebookLoginButton.setReadPermissions(
-            getString(R.string.fb_permission_profile),
-            getString(R.string.fb_permission_friends)
-      );
-//      mFacebookLoginButton.setPublishPermissions(
-//            getString(R.string.fb_permission_publish)
-//      );
-
-      mFacebookLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
-         @Override
-         public void onSuccess(LoginResult loginResult) {
-            System.out.println("Success! Logged in with access token: " + loginResult.getAccessToken());
-            // Handle Facebook Login success
-            // Send user e-mail and other info to server?
-            // Send some access token?
-            SharedPreferences.Editor editor = mPreferences.edit();
-            editor.putBoolean(LOGGED_IN, true);
-            editor.putBoolean(FACEBOOK_LOGIN, true);
-            editor.putBoolean(GOOGLE_SIGN_IN, false);
-            editor.apply();
-
-            returnToMain();
-         }
-
-         @Override
-         public void onCancel() {
-            System.out.println("Login canceled.");
-         }
-
-         @Override
-         public void onError(FacebookException error) {
-            System.out.println(error.toString());
-         }
-      });
-
-      // Init Google Login
-      mGoogleApiClient = GoogleApiClientService.getInstance(this);
+   @Override
+   protected void onDestroy() {
+      super.onStop();
+      mAuthManager.unregisterCallback();
    }
 
    @OnClick(R.id.google_sign_in_button)
    public void googleLogin(View view) {
-      Intent googleIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-      startActivityForResult(googleIntent, RC_SIGN_IN);
-   }
-
-   private void handleGoogleSignInResult(GoogleSignInResult result) {
-      if (result.isSuccess()) {
-         GoogleSignInAccount account = result.getSignInAccount();
-
-         if (account != null) {
-            System.out.println(account.getEmail());
-         }
-
-         // Handle Google Sign In success
-         // Send user e-mail and other info to server?
-         // Send some access token?
-         SharedPreferences.Editor editor = mPreferences.edit();
-         editor.putBoolean(LOGGED_IN, true);
-         editor.putBoolean(FACEBOOK_LOGIN, false);
-         editor.putBoolean(GOOGLE_SIGN_IN, true);
-         editor.apply();
-
-         returnToMain();
-      }
-      else {
-         System.out.println("Google login failed :(");
-      }
+      startActivityForResult(mAuthManager.getGoogleIntent(), RC_SIGN_IN);
    }
 
    @Override
@@ -151,15 +72,10 @@ public class LoginActivity extends AppCompatActivity {
       super.onActivityResult(requestCode, resultCode, data);
 
       if (requestCode == RC_SIGN_IN) {
-         GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-         handleGoogleSignInResult(result);
+         mAuthManager.googleActivityResult(data);
       }
-
-      mCallbackManager.onActivityResult(requestCode, resultCode, data);
-   }
-
-   private void returnToMain() {
-      Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-      startActivity(mainIntent);
+      else {
+         mAuthManager.facebookActivityResult(requestCode, resultCode, data);
+      }
    }
 }
