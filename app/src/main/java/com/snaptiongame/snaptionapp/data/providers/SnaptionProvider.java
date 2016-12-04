12f94014db38495030b8b6d86b2ab54eb2ed1da9
Tyler Wong <tbwong3@gmail.com>
2016-12-03 17:18:40 -0800

    Added game view with temporary data

--- app/src/main/java/com/snaptiongame/snaptionapp/data/providers/SnaptionProvider.java
+++ app/src/main/java/com/snaptiongame/snaptionapp/data/providers/SnaptionProvider.java
@@ -4,6 +4,7 @@ import com.snaptiongame.snaptionapp.data.models.Snaption;
 import com.snaptiongame.snaptionapp.data.providers.api.SnaptionApiProvider;
 import com.snaptiongame.snaptionapp.data.services.SnaptionApiService;
 
+import java.util.Collections;
 import java.util.List;
 
 import rx.Observable;
@@ -15,13 +16,25 @@ import rx.Observable;
 public class SnaptionProvider {
    private static SnaptionApiService apiService = SnaptionApiProvider.getApiService();
 
-   public static Observable<List<Snaption>> getAllSnaptions() {
-      return apiService.getSnaptions()
-               .compose(o -> o.subscribeOn(SnaptionApiProvider.getNetworkScheduler()));
+   private static Observable.Transformer<List<Snaption>, List<Snaption>> sortSnaptions() {
+      return (Observable<List<Snaption>> o) -> o.filter(snaptions -> {
+         Collections.reverse(snaptions);
+         return true;
+      });
    }
 
-   public static void addSnaption(Snaption snaption) {
-      apiService.addSnaption(snaption)
-               .compose(o -> o.subscribeOn(SnaptionApiProvider.getNetworkScheduler()));
+   protected static Observable.Transformer<List<Snaption>, List<Snaption>> mockSnaptions() {
+      return null;
+   }
+
+   public static Observable<List<Snaption>> getAllSnaptions() {
+      return apiService.getSnaptions()
+               .compose(o -> o.subscribeOn(SnaptionApiProvider.getNetworkScheduler()))
+               .compose(sortSnaptions());
+   }
+
+   public static void addSnaption(String type, String image) {
+      apiService.addSnaption(type, image)
+            .compose(o -> o.subscribeOn(SnaptionApiProvider.getNetworkScheduler()));
    }
 }
