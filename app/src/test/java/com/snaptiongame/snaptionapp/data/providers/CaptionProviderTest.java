62ff4b453c260acb88830d0958b0b3db2e66c5c4
Tyler Wong <tbwong3@gmail.com>
2017-02-08 23:02:00 -0800

    Add CaptionProviderTest

---
+++ app/src/test/java/com/snaptiongame/snaptionapp/data/providers/CaptionProviderTest.java
@@ -0,0 +1,80 @@
+package com.snaptiongame.snaptionapp.data.providers;
+
+import com.snaptiongame.snaptionapp.data.models.Caption;
+import com.snaptiongame.snaptionapp.data.models.Like;
+import com.snaptiongame.snaptionapp.data.services.SnaptionApiService;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Tyler Wong
+ */
+
+public class CaptionProviderTest {
+   private SnaptionApiService service;
+   private List<Caption> captions;
+   private Caption caption;
+   private Like like;
+
+   private static final int TEST_GAME_ID = 1;
+
+   @Before
+   public void setup() {
+      service = mock(SnaptionApiService.class);
+      captions = new ArrayList<>();
+      captions.add(new Caption(0, "test0"));
+      captions.add(new Caption(1, "test1"));
+      captions.add(new Caption(2, "test2"));
+      captions.add(new Caption(3, "test3"));
+      captions.add(new Caption(4, "test4"));
+      when(service.getCaptions(TEST_GAME_ID)).thenReturn(Observable.just(captions));
+      like = new Like(0, 0, false, false, "");
+      when(service.upvoteCaption(like)).thenReturn(Observable.just(like));
+      caption = new Caption(0, "test0");
+      when(service.addCaption(TEST_GAME_ID, caption)).thenReturn(Observable.just(caption));
+   }
+
+   @Test
+   public void testGetCaptions() {
+      CaptionProvider.getCaptions(TEST_GAME_ID)
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                  returnedCaptions -> assertTrue(returnedCaptions.containsAll(captions)),
+                  e -> {},
+                  () -> {}
+            );
+   }
+
+   @Test
+   public void testUpvoteCaption() {
+      CaptionProvider.upvoteCaption(like)
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                  returnedLike -> assertTrue(returnedLike.equals(like)),
+                  e -> {},
+                  () -> {}
+            );
+   }
+
+   @Test
+   public void testAddCaption() {
+      CaptionProvider.addCaption(TEST_GAME_ID, caption)
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                  returnedCaption -> assertTrue(returnedCaption.equals(caption)),
+                  e -> {},
+                  () -> {}
+            );
+   }
+}
