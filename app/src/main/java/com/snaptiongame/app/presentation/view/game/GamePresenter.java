3d367b574123c485ac94f91de90fdd1cbb810cb3
Nicolas Romero <nromo01@gmail.com>
2017-04-17 16:54:05 -0700

    Made getContext change and removed unused import

--- app/src/main/java/com/snaptiongame/app/presentation/view/game/GamePresenter.java
+++ app/src/main/java/com/snaptiongame/app/presentation/view/game/GamePresenter.java
@@ -1,12 +1,12 @@
 package com.snaptiongame.app.presentation.view.game;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.snaptiongame.app.R;
+import com.snaptiongame.app.SnaptionApplication;
 import com.snaptiongame.app.data.models.Caption;
 import com.snaptiongame.app.data.models.CaptionSet;
 import com.snaptiongame.app.data.models.DeepLinkRequest;
@@ -108,7 +108,7 @@ public class GamePresenter implements GameContract.Presenter {
                         e -> {
                             Timber.e(e);
                             mGameView.setRefreshing(false);
-                            Toast.makeText((GameActivity) mGameView,
+                            Toast.makeText(SnaptionApplication.getContext(),
                                     ((GameActivity) mGameView)
                                             .getResources()
                                             .getString(R.string.failed_caption_submission),
