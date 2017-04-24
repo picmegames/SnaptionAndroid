1cb07455e63c23ceec6db37d4c1827e49ca40ba9
Tyler Wong <tbwong3@gmail.com>
2017-04-24 15:01:38 -0700

    Fixed build

--- app/src/test/java/com/snaptiongame/app/data/models/FriendsTest.java
+++ app/src/test/java/com/snaptiongame/app/data/models/FriendsTest.java
@@ -1,6 +1,6 @@
 package com.snaptiongame.app.data.models;
 
-import com.snaptiongame.app.presentation.view.friends.FriendsFragment;
+import com.snaptiongame.app.presentation.view.friends.FriendsPresenter;
 
 import org.junit.Test;
 
@@ -20,37 +20,37 @@ public class FriendsTest {
     @Test
     public void filter_isCorrect() throws Exception {
         List<Friend> inst = new ArrayList<>(), check;
-        assertEquals(FriendsFragment.filterList(inst, "some filter category"), new
+        assertEquals(FriendsPresenter.filterList(inst, "some filter category"), new
                 ArrayList<Friend>());
         inst.add(new Friend(1, "John", "Cena", "John Cena", "jcena1234", "", "", ""));
         inst.add(new Friend(2, "Steve", "Buscemi", "Steve Buscemi", "raddude17", "", "", ""));
         inst.add(new Friend(4, "Steve", "Cenamopolis", "Steve Cenamopolis", "somefakeperson",
                 "", "", ""));
 
-        check = FriendsFragment.filterList(inst, "Steve");
+        check = FriendsPresenter.filterList(inst, "Steve");
         assertTrue(check.get(0).fullName.equals("Steve Buscemi"));
         assertTrue(check.get(1).fullName.equals("Steve Cenamopolis"));
         assertEquals(check.size(), 2);
 
         //check last name match
-        check = FriendsFragment.filterList(inst, "cena");
+        check = FriendsPresenter.filterList(inst, "cena");
         assertTrue(check.get(0).fullName.equals("John Cena"));
         assertTrue(check.get(1).fullName.equals("Steve Cenamopolis"));
         assertEquals(check.size(), 2);
 
         //check not in list
         check.clear();
-        assertEquals(FriendsFragment.filterList(inst, "zzzz"), new ArrayList<Friend>());
+        assertEquals(FriendsPresenter.filterList(inst, "zzzz"), new ArrayList<Friend>());
 
         //check both first and last
-        check = FriendsFragment.filterList(inst, "hn ce");
+        check = FriendsPresenter.filterList(inst, "hn ce");
         assertTrue(check.get(0).fullName.equals("John Cena"));
         assertEquals(check.size(), 1);
 
         //check empty queries
-        check = FriendsFragment.filterList(inst, "");
+        check = FriendsPresenter.filterList(inst, "");
         assertEquals(check, inst);
-        check = FriendsFragment.filterList(inst, null);
+        check = FriendsPresenter.filterList(inst, null);
         assertEquals(check, inst);
     }
 }
