82c737f18300e1684151fe17f71f036b647b6126
Nicolas Romero <nromo01@gmail.com>
2017-04-26 22:37:12 -0700

    Issues with disposables and friend network calls

--- app/src/main/java/com/snaptiongame/app/data/converters/FriendConverter.java
+++ app/src/main/java/com/snaptiongame/app/data/converters/FriendConverter.java
@@ -27,17 +27,18 @@ public class FriendConverter implements JsonSerializer<Friend>, JsonDeserializer
     @Override
     public Friend deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
             throws JsonParseException {
-        JsonObject object = json.getAsJsonObject();
-        Friend newFriend = new Friend();
 
-        newFriend.id = object.get(User.ID).getAsInt();
-        newFriend.username = object.get(User.USERNAME).getAsString();
+            JsonObject object = json.getAsJsonObject();
+            Friend newFriend = new Friend();
 
-        JsonObject pictureObject = object.getAsJsonObject(User.PICTURE);
-        newFriend.imageUrl = pictureObject.get(User.IMAGE_URL).getAsString();
-        newFriend.imageWidth = pictureObject.get(User.IMAGE_WIDTH).getAsInt();
-        newFriend.isSnaptionFriend = true;
-        newFriend.imageHeight = pictureObject.get(User.IMAGE_HEIGHT).getAsInt();
-        return newFriend;
+            newFriend.id = object.get(User.ID).getAsInt();
+            newFriend.username = object.get(User.USERNAME).getAsString();
+
+            JsonObject pictureObject = object.getAsJsonObject(User.PICTURE);
+            newFriend.imageUrl = pictureObject.get(User.IMAGE_URL).getAsString();
+            newFriend.imageWidth = pictureObject.get(User.IMAGE_WIDTH).getAsInt();
+            newFriend.isSnaptionFriend = true;
+            newFriend.imageHeight = pictureObject.get(User.IMAGE_HEIGHT).getAsInt();
+            return newFriend;
     }
 }
