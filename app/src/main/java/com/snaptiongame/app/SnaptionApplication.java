18d5f7395bfe3dd8effd7a220cc00e7cdea9f5fe
Tyler Wong <tbwong3@gmail.com>
2017-05-13 15:49:47 -0700

    Removing leak canary

--- app/src/main/java/com/snaptiongame/app/SnaptionApplication.java
+++ app/src/main/java/com/snaptiongame/app/SnaptionApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 
 import com.snaptiongame.app.data.auth.AuthManager;
-import com.squareup.leakcanary.LeakCanary;
 
 import io.branch.referral.Branch;
 import timber.log.Timber;
@@ -30,13 +29,6 @@ public class SnaptionApplication extends Application {
 
         // INIT Authentication Manager
         AuthManager.init(context);
-
-        // INIT Leak Canary (Memory leak checking)
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return;
-        }
-
-        LeakCanary.install(this);
 
         if (BuildConfig.DEBUG) {
             // INIT Timber (Logger for debug builds)
