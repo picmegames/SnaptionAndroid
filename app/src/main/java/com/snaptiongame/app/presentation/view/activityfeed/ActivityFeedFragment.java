326924ccd4a9fa845d772b18f41155fb87b3fac8
Tyler Wong <tbwong3@gmail.com>
2017-05-26 00:52:23 -0700

    Remove test data

--- app/src/main/java/com/snaptiongame/app/presentation/view/activityfeed/ActivityFeedFragment.java
+++ app/src/main/java/com/snaptiongame/app/presentation/view/activityfeed/ActivityFeedFragment.java
@@ -57,52 +57,6 @@ public class ActivityFeedFragment extends Fragment implements ActivityFeedContra
         mUnbinder = ButterKnife.bind(this, view);
         mPresenter = new ActivityFeedPresenter(this);
 
-        // TESTING
-        List<ActivityFeedItem> testItems = new ArrayList<>();
-//        User friend1 = new User();
-//        friend1.imageUrl = "https://img.pokemondb.net/artwork/bulbasaur.jpg";
-//        friend1.username = "bulbasaur";
-//        Game game1 = new Game();
-//        game1.imageUrl = "https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F13%2F39a6deb0-8ff2-4ec5-854b-1b197d81df0a.jpg";
-//        User friend2 = new User();
-//        friend2.imageUrl = "http://vignette4.wikia.nocookie.net/pokemon/images/5/5f/025Pikachu_OS_anime_11.png/revision/latest?cb=20150717063951";
-//        friend2.username = "pikachu";
-//        Game game2 = new Game();
-//        game2.imageUrl = "https://vignette2.wikia.nocookie.net/pokemon/images/7/74/Red_Mewtwo_PO.png/revision/latest?cb=20141008193632";
-//        User friend3 = new User();
-//        friend3.imageUrl = "https://img.pokemondb.net/artwork/squirtle.jpg";
-//        friend3.username = "squirtle";
-//        Game game3 = new Game();
-//        game3.imageUrl = "http://cdn.bulbagarden.net/upload/thumb/b/b1/Misty_AG.png/250px-Misty_AG.png";
-//        User friend4 = new User();
-//        friend4.imageUrl = "http://vignette4.wikia.nocookie.net/pokemon/images/5/5f/025Pikachu_OS_anime_11.png/revision/latest?cb=20150717063951";
-//        friend4.username = "ash_ketchum";
-//        friend4.fullName = "Ash Ketchum";
-//        Game game4 = new Game();
-//        game4.imageUrl = "https://vignette2.wikia.nocookie.net/pokemon/images/7/74/Red_Mewtwo_PO.png/revision/latest?cb=20141008193632";
-//        ActivityFeedItem item1 = new ActivityFeedItem(0, 1495702207, ActivityFeedUtils.CAPTIONED_GAME,
-//                null, friend1, game1, null);
-//        ActivityFeedItem item2 = new ActivityFeedItem(0, 1495270189, ActivityFeedUtils.FRIENDED_YOU,
-//                null, friend2, game2, null);
-//        ActivityFeedItem item3 = new ActivityFeedItem(0, 1495700449, ActivityFeedUtils.FRIEND_MADE_GAME,
-//                null, friend3, game3, null);
-//        ActivityFeedItem item4 = new ActivityFeedItem(0, 1495710449, ActivityFeedUtils.NEW_FACEBOOK_FRIEND,
-//                null, friend4, game4, null);
-//        ActivityFeedItem item5 = new ActivityFeedItem(0, 1495700449, ActivityFeedUtils.FRIEND_INVITED_GAME,
-//                null, friend3, game3, null);
-//        testItems.add(item1);
-//        testItems.add(item2);
-//        testItems.add(item3);
-//        testItems.add(item2);
-//        testItems.add(item1);
-//        testItems.add(item2);
-//        testItems.add(item3);
-//        testItems.add(item1);
-//        testItems.add(item4);
-//        testItems.add(item4);
-//        testItems.add(item5);
-//        testItems.add(item5);
-
         mDecoration = new InsetDividerDecoration(
                 ActivityFeedItemViewHolder.class,
                 getResources().getDimensionPixelSize(R.dimen.divider_height),
@@ -110,7 +64,7 @@ public class ActivityFeedFragment extends Fragment implements ActivityFeedContra
                 ContextCompat.getColor(getContext(), R.color.divider));
         mActivityFeed.addItemDecoration(mDecoration);
 
-        mAdapter = new ActivityFeedAdapter(testItems);
+        mAdapter = new ActivityFeedAdapter(new ArrayList<>());
         mActivityFeed.setAdapter(mAdapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         mActivityFeed.setLayoutManager(layoutManager);
