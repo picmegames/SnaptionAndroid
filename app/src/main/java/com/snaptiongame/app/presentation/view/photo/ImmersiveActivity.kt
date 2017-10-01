1821939579d9d15fc3fc8e355122a7ec4c860359
Tyler Wong <tbwong3@gmail.com>
2017-10-01 13:08:29 -0700

    Convert to Kotlin

---
+++ app/src/main/java/com/snaptiongame/app/presentation/view/photo/ImmersiveActivity.kt
@@ -0,0 +1,46 @@
+package com.snaptiongame.app.presentation.view.photo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.request.RequestOptions
+import com.snaptiongame.app.R
+
+import kotlinx.android.synthetic.main.activity_immersive.*
+
+/**
+ * @author Tyler Wong
+ */
+
+class ImmersiveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_immersive)
+
+        photoView.setOnLongClickListener { _ ->
+            super.onBackPressed()
+            true
+        }
+
+        val intent = intent
+        val imageUrl = intent.getStringExtra(IMAGE_URL)
+
+        if (imageUrl != null && !imageUrl.isEmpty()) {
+            val options = RequestOptions()
+                    .dontAnimate()
+                    .priority(Priority.IMMEDIATE)
+
+            Glide.with(this)
+                    .load(imageUrl)
+                    .apply(options)
+                    .into(photoView)
+        }
+    }
+
+    companion object {
+        const val IMAGE_URL = "imageUrl"
+    }
+}
