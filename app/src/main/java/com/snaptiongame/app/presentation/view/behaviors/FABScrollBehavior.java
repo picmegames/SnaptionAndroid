904f9bd3f00ec89a93edeb9c447b8b67351158ca
Tyler Wong <tbwong3@gmail.com>
2017-10-01 12:59:58 -0700

    Remove unused import

--- app/src/main/java/com/snaptiongame/app/presentation/view/behaviors/FABScrollBehavior.java
+++ app/src/main/java/com/snaptiongame/app/presentation/view/behaviors/FABScrollBehavior.java
@@ -11,7 +11,6 @@ import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorCompat;
 import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Interpolator;
